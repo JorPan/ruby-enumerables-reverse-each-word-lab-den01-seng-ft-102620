d21d60af035dd0f4e4fd21b35fe941baea5c32d4
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-12 17:15:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   words = string.split(" ")
   new_words = []
-  words.each { |word| new_words.push(word.reverse) }
+  words.collect { |word| new_words.push(word.reverse) }
   new_sent = new_words.join(" ")
   return new_sent
 end
